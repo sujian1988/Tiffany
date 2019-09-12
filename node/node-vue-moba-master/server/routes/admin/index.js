6e696158bf9aab5d3bceee68a2ededa84da30af1
sujian1988 <597201889@qq.com>
2019-09-12 17:28:05 +0900

    添加视频创建接口

--- node/node-vue-moba-master/server/routes/admin/index.js
+++ node/node-vue-moba-master/server/routes/admin/index.js
@@ -144,16 +144,22 @@ app.post('/admin/api/app_create_user', async (req, res) => {
 
 })
 
-const video = require('../../modles/Video')
+
 app.get('/admin/api/app_video_list', async(req, res) =>{
+  const video = require('../../modles/Video')
   const videos = await video.find().limit(10) // 限制10条数据
   res.status(200).json({
       videos
   });
 })
 
+//创建视频
+ app.post('/admin/api/app_create_video', async (req, res) => {
+  const video = require('../../modles/Video')
+  const newVideo = await video.create(req.body)
+  res.status(200).json({newVideo});
 
-
+})
 
 
 
