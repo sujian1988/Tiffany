c3d3bf64de80fd767a5ca3a40b54c15a0a93250c
sujian1988 <597201889@qq.com>
2019-09-25 21:40:10 +0900

    添加直播ui

---
+++ node/node-vue-moba-master/server/modles/Live.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')\
+const schema = new mongoose.Schema({
+    mliveId: { type: String},
+    mliveTitle: {type: String},
+    mHosterId: { type: String},
+    mRtmpPushUrl: { type: String},
+    mRtmpPullUrl: { type: String},
+    mHlsUrl: { type: String},
+    mLiveTopic: { type: String},
+    mMemNumber: { type: Number},
+    headerUrl: { type: String},
+    bgImageUrl: {type: String},
+    nickName: { type: String},
+})
+
+module.exports = mongoose.model('Live', schema)
