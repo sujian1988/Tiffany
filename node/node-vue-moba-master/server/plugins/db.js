d0523f16668d97cf7b17b0f7c1f0a47747b4a345
sujian1988 <597201889@qq.com>
2019-10-21 19:50:23 +0900

    test back

--- node/node-vue-moba-master/server/plugins/db.js
+++ node/node-vue-moba-master/server/plugins/db.js
@@ -1,14 +1,10 @@
 
 module.exports = app => {
 
-  var options = {
-    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
-    };
   const mongoose = require("mongoose")
 
   //  mongoose.connect('mongodb://localhost:27017/node-vue-moba',{ 
-  mongoose.connect('mongodb://127.0.0.1:27017/node-vue-moba', options /*{useNewUrlParser: true}*/)
+  mongoose.connect('mongodb://127.0.0.1:27017/node-vue-moba', {useNewUrlParser: true})
 
   require('require-all')(__dirname + '/../modles')
 }
